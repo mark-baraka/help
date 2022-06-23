65c717f51aa0477718235bbb89172172267817e8
Mark-Baraka <barakaanderson24@gmail.com>
2022-06-23 21:03:38 +0300

    My first repo today

---
+++ new.py
@@ -0,0 +1,7 @@
+# this is a commentprint("am learnining")
+def area_of_a_circle(radius):
+    pi=3.142
+    radius=input("please enter the radius")
+    r=int(radius)
+    return pi*r**2
+area_of_a_circle()
